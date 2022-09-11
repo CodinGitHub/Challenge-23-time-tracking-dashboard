d984f6ebf244db7d074f4e4bdb3c617339cc2c6d
CodinGitHub <gms2programador@gmail.com>
2022-09-11 06:34:20 -0500

    drawing at start

--- main.js
+++ main.js
@@ -1,72 +1,75 @@
-import data from './data.json' assert {type: 'json'}
+import data from './data.json' assert {type: 'json'};
 
-let BgColors = [
-    {activity: 'work', color: 'hsl(15, 100%, 70%)'},
-    {activity: 'play', color: 'hsl(195, 74%, 62%)'},
-    {activity: 'study', color: 'hsl(348, 100%, 68%)'},
-    {activity: 'exercise', color: 'hsl(145, 58%, 55%)'},
-    {activity: 'social', color: 'hsl(264, 64%, 52%)'},
-    {activity: 'self care', color: 'hsl(43, 84%, 65%)'},
+console.log(data)
+
+let bgColors = [
+    'hsl(15, 100%, 70%)',
+    'hsl(195, 74%, 62%)',
+    'hsl(348, 100%, 68%)',
+    'hsl(145, 58%, 55%)',
+    'hsl(264, 64%, 52%)',
+    'hsl(43, 84%, 65%)',
 ]
 
-let dailyArray = data.map(item => item.timeframes.daily);
-let weeklyArray = data.map(item => item.timeframes.weekly);
-let monthlyArray = data.map(item => item.timeframes.monthly);
+let daylyArray = data.map(item => item.timeframes.daily)
+let weeklyArray = data.map(item => item.timeframes.weekly)
+let monthlyArray = data.map(item => item.timeframes.monthly)
+
+console.log(daylyArray)
+console.log(weeklyArray)
+console.log(monthlyArray)
 
 let dailyBtn = document.querySelector('#daily');
 let weeklyBtn = document.querySelector('#weekly');
 let monthlyBtn = document.querySelector('#monthly');
 
-let secondSection = document.querySelector('.second-section')
+let secondSection =  document.querySelector('.second-section');
 
-data.forEach(element=>{
-    console.log(element.title.toLocaleLowerCase())
+drawElements(daylyArray)
+
+dailyBtn.addEventListener('click', ()=>{
+    drawElements(daylyArray)
 });
 
-
-dailyBtn.addEventListener('click', ()=>{    
-    drawElements(dailyArray);
+weeklyBtn.addEventListener('click', ()=>{
+    drawElements(weeklyArray)
 });
 
-weeklyBtn.addEventListener('click', ()=>{    
-    drawElements(weeklyArray);
-});
-
-monthlyBtn.addEventListener('click', ()=>{    
-    drawElements(monthlyArray);
+monthlyBtn.addEventListener('click', ()=>{
+    drawElements(monthlyArray)
 });
 
 function drawElements(array){
     secondSection.innerHTML = '';
-    array.forEach((element, index) => {
-        let activity = data[index].title;
-        let activityLowerCase = activity.toLocaleLowerCase();
-        
-        //Buscar el color
-        let colorObject = BgColors.find(element => element.activity === activityLowerCase)
-        console.log()
+    array.forEach( (element, index) =>{
+        // console.log(element)
 
-        if(activityLowerCase === 'self care'){
-            activityLowerCase = 'self-care'
+        let title = data[index].title;
+        let titleLowerCase = title.toLocaleLowerCase()
+
+        if(titleLowerCase == 'self care'){
+            titleLowerCase = 'self-care'
         }
 
-        
+        console.log(title)
+        console.log(titleLowerCase)
+
         secondSection.innerHTML += `
         <div class="card">
-            <div class="card__background" style="background-color: ${colorObject.color};">
-                <img class="card__background--image" src="./images/icon-${activityLowerCase}.svg" alt="">
+            <div class="card__background" style="background-color: ${bgColors[index]};">
+            <img class="card__image" src="./images/icon-${titleLowerCase}.svg" alt="">
             </div>
             <div class="card__details">
-                <div class="card__up">
-                <p class="card__title">${activity}</p>
-                <img class="card__dots" src="./images/icon-ellipsis.svg" alt="dots">
-                </div>
-                <div class="card__down">
-                <p class="card__hours">${element.current}hrs</p>
-                <p class="card__state">Previous - ${element.previous}hrs</p>
-                </div>
+            <div class="card__activity">
+                <p class="card__title">${title}</p>
+                <img class="card__dots" src="./images/icon-ellipsis.svg" alt="three dots">
+            </div>
+            <div class="card__time">
+                <p class="card__hour">${element.current}hrs</p>
+                <p class="card__previous">Previous - ${element.previous}hrs</p>
+            </div>
             </div>
         </div>
         ` 
-    });
+    } )
 }
